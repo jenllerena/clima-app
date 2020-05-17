ce6ca0894caefdfbd2ca9e47488dace99001104c
Jenny <jennyller22@gmail.com>
2020-05-17 04:44:43 -0500

    maquetacion

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -14,6 +14,6 @@ export class WeatherService {
    }
 
    getWeather(cityName){
-     return this.httpClient.get(`${this.url}${cityName}`);
+     return this.httpClient.get(`${this.url}${cityName}&units=metric`);
    }
 }
