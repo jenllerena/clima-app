2f0d6b79e7d07e52aac8b6e4ebcf6fbe6f43ffff
Jenny <jennyller22@gmail.com>
2020-05-17 23:03:02 -0500

    diseño, validacion cuando no hay data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {WeatherService } from './services/weather.service';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from './services/weather.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,20 +8,65 @@ import {WeatherService } from './services/weather.service';
 })
 
 export class AppComponent implements OnInit {
-  weatherData:any;
+  weatherData: any;
   temperature;
- constructor(private weatherService:WeatherService){}
-  ngOnInit(){
-    
+  icon;
+  cityName;
+  country;
+  feelslLike;
+  pressure; humidity;
+  description;
+  tempMin;
+  tempMax;
+
+  constructor(private weatherService: WeatherService) {
   }
-  getWeather(cityName){
+
+  ngOnInit() {
+
+  }
+
+  getWeather(cityName) {
+    if (!cityName) {
+      alert("ingrese una ciudad")
+      return;
+    }
     console.log(cityName)
     this.weatherService.getWeather(cityName)
-    .subscribe(
-      res=>this.weatherData=res
-    )
+      .subscribe(
+        res => {
+          this.weatherData = res
+          this.saveData(res);
+        },
+        error1 => alert("No se encontró informacion")
+      );
   }
-  searchByCity(city:HTMLInputElement){
+
+  saveData = (data) => {
+    this.temperature = Math.round(data.main.temp);
+    this.feelslLike = Math.round(data.main.feels_like);
+    this.tempMin = data.main.temp_min;
+    this.tempMax = data.main.temp_max;
+    this.description = data.weather[0].description;
+    this.pressure = data.main.pressure;
+    this.humidity = data.main.humidity;
+    this.cityName = data.name;
+    this.country = data.sys.country;
+    this.icon = data.weather[0].icon;
+  }
+
+  getIcon(id) {
+    var url = `http://openweathermap.org/img/wn/${id}@2x.png`
+    return url;
+  }
+
+  createIcon = () => {
+    const url = this.getIcon(this.icon);
+    const image = document.getElementById('img-icon');
+    image.setAttribute('src', url);
+  }
+
+  searchByCity(city: HTMLInputElement) {
     this.getWeather(city.value);
     city.value = '';
 
